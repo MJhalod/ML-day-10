39c97acc0c983f14eee30da13946ec7a50c4c5b2
MJhalod <125639197+MJhalod@users.noreply.github.com>
2023-12-27 22:36:28 +0530

    Add files via upload

---
+++ ML day 10/cancer.py
@@ -0,0 +1,33 @@
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+data = pd.read_csv('cancer.csv')  
+
+print(data.info())
+
+
+
+X = data.drop('target_variable', axis=1)  
+y = data['target_variable']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(random_state=42)  
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
+
+print('Classification Report:')
+print(classification_report(y_test, y_pred))
+
+
+
+
